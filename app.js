5447620815f1fc973c9ed461347b5904264bd514
Nicolas PLOQUIN <nicolas.ploquin@gmail.com>
2022-03-11 09:57:08 +0100

    add first steps JS samples

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 
 console.log('Mon premier script JavaScript !');
 
+let message = 'quelque chose';
+
+console.log(message);
+
+// message = message.toUpperCase();
+
+console.log(message);
+
 
 /** @type HTMLHeadingElement */
 let elemTitre = document.querySelector('header h1');
@@ -11,6 +19,19 @@ function titreClickCss(){
     elemTitre.style.fontWeight = 'bold';
 }
 function titreClick(){
+    let nb = 1;
+    console.log('nombre = ' + nb);
+    console.log(`nombre = ${nb}`);
+    
+    
+    if(true){
+        let nb = 2;
+        console.log(nb);        
+    }
+    
+    console.log(nb);        
+
+
     elemTitre.textContent = 'Banqu<sup>e</sup>';
     // elemTitre.innerHTML = 'Banqu<sup>e</sup>';
     elemTitre.parentNode.classList.toggle('nouveau'); /* add, remove, toggle, contains */
@@ -32,3 +53,76 @@ JS
 - Rechercher l'élément sidebar et applique la classe closed
 
 */
+
+let btnMenu = document.querySelector('#btn-menu-close');
+let menu = document.querySelector('body > nav.sidebar');
+
+btnMenu.addEventListener('click', reduireMenu);
+
+function reduireMenu(){
+    const btnMenuLabel = btnMenu.querySelector('.material-icons');
+    menu.classList.toggle('small');
+    if(menu.classList.contains('small')){
+        btnMenuLabel.textContent = 'menu';
+    }else{
+        btnMenuLabel.textContent = 'clear';
+    }
+
+}
+
+/* ------ Formulaire ------ */
+
+/** @type HTMLFormElement */
+const formClient = document.querySelector('#form-client');
+
+/** @type HTMLInputElement */
+const inputNom = formClient.querySelector('[name="nom"]');
+/** 
+ * Champ email
+ * @type HTMLInputElement 
+ */
+const inputEmail = formClient.querySelector('[name="email"]');
+/** @type HTMLInputElement */
+const inputEmailVerif = formClient.querySelector('[name="email-verif"]');
+
+// 1 -
+inputNom.addEventListener('input', (event) => {
+    inputNom.value = inputNom.value.toUpperCase();
+});
+
+// // 2 -
+// inputNom.addEventListener('input', function (){
+//     inputNom.value = inputNom.value.toUpperCase();
+// });
+
+// // 3 -
+// inputNom.addEventListener('input',transformeNom );
+// function transformeNom(){
+//     inputNom.value = inputNom.value.toUpperCase();
+// }
+
+
+// inputNom.setCustomValidity('Il y a une erreur dans cette valeur !');
+// inputNom.setCustomValidity('');
+
+// formClient.checkValidity();
+// formClient.reportValidity();
+
+
+inputEmail.addEventListener('input', checkEmail);
+inputEmailVerif.addEventListener('input', checkEmail);
+
+/**
+ * Vérifie que les deux adresse e-mail saisies sont identiques.
+ * @param {Event} event objet décrivant l'événement HTML généré
+ * @returns void 
+ */
+function checkEmail(event) {
+    const email = inputEmail.value.trim();
+    const emailVerif = inputEmailVerif.value.trim();
+    if(email !== emailVerif){
+        inputEmailVerif.setCustomValidity('Les deux adresses email doivent être identiques.');
+    } else {
+        inputEmailVerif.setCustomValidity('');
+    }
+}
